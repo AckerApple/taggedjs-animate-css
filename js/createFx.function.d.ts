05acc0b6fe9972117bf7319a9838292b2730090a
Acker Apple <y>
2024-04-30 12:14:16 -0400

    dist patch

---
+++ js/createFx.function.d.ts
@@ -0,0 +1,14 @@
+type ElementEvent = {
+    target: HTMLElement;
+    stagger: number;
+};
+export declare function createFx({ fxIn, fxOut, staggerBy, }: {
+    fxIn: string;
+    fxOut: string;
+    staggerBy?: number;
+}): {
+    in: (input: ElementEvent) => Promise<void>;
+    out: (input: ElementEvent) => Promise<void>;
+};
+export declare function captureElementPosition(element: any): void;
+export {};
--- ts/index.ts
+++
@@ -1,3 +0,0 @@
-import { createFx } from "./createFx.function"
-
-export const { in: fadeInDown, out: fadeOutUp } = createFx({fxIn:'fadeInDown', fxOut:'fadeOutUp'})
